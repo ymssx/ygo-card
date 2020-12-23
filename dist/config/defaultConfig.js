e5d3a3c81d8a23c95a5a57b32a40cefcd906a8fd
Yami <ymssx@qq.com>
2020-12-23 11:58:49 +0800

    build

--- dist/config/defaultConfig.js
+++ dist/config/defaultConfig.js
@@ -1,134 +1,186 @@
 // 默认中文模板
 
 export default {
-  "moldName": "default",
-  "fonts": {
-    "cn": {
-      "name": "cn.ttf",
-      "type": "relative"
+  moldName: "default",
+  fonts: {
+    cn: {
+      name: "cn.ttf",
+      type: "relative",
     },
-    "number": {
-      "name": "number.ttf",
-      "type": "relative"
+    number: {
+      name: "number.ttf",
+      type: "relative",
     },
-    "link": {
-      "name": "link.ttf",
-      "type": "relative"
+    link: {
+      name: "link.ttf",
+      type: "relative",
     },
-    "password": {
-      "name": "password.ttf",
-      "type": "relative"
-    }
+    password: {
+      name: "password.ttf",
+      type: "relative",
+    },
   },
-  "style": {
-    "moldSize": [813, 1185],
-    "name": {
-      "font": "cn",
-      "fontSize": 65,
-      "maxWidth": 610,
-      "position": [65, 96]
+  style: {
+    moldSize: [813, 1185],
+    name: {
+      font: "cn",
+      fontSize: 65,
+      maxWidth: 610,
+      position: [65, 96],
     },
-    "pic": {
-      "position": [101, 220, 614, 616],
-      "position_lb": [57, 213, 702, 528]
+    pic: {
+      position: [101, 220, 614, 616],
+      position_lb: [57, 213, 702, 528],
     },
-    "attribute": {
-      "position": [680, 57, 75, 75],
-      "size": [66, 66]
+    attribute: {
+      position: [680, 57, 75, 75],
+      size: [66, 66],
     },
-    "level": {
-      "position": [686, 145],
-      "size": [50, 50],
-      "distance": 55
+    level: {
+      position: [686, 145],
+      size: [50, 50],
+      distance: 55,
     },
-    "monsterDesc": {
-      "splitMode": "cn",
-      "font": "cn",
-      "fontSize": 24,
-      "lbFontSize": 22,
-      "position": [66, 942],
-      "lbPosition": [130, 773],
-      "lineHeight": 26,
-      "lbLineHeight": 24.5,
-      "maxLines": 6,
-      "maxWidth": 683
+    monsterDesc: {
+      splitMode: "cn",
+      font: "cn",
+      fontSize: 24,
+      lbFontSize: 22,
+      position: [66, 942],
+      lbPosition: [130, 773],
+      lineHeight: 26,
+      lbLineHeight: 24.5,
+      maxLines: 6,
+      maxWidth: 683,
     },
-    "spellDesc": {
-      "splitMode": "cn",
-      "font": "cn",
-      "fontSize": 24,
-      "position": [66, 915],
-      "lineHeight": 24,
-      "maxLines": 9,
-      "maxWidth": 683
+    spellDesc: {
+      splitMode: "cn",
+      font: "cn",
+      fontSize: 24,
+      position: [66, 915],
+      lineHeight: 24,
+      maxLines: 9,
+      maxWidth: 683,
     },
-    "race": {
-      "font": "cn",
-      "fontSize": 26,
-      "position": [53, 915],
-      "maxWidth": 610
+    race: {
+      font: "cn",
+      fontSize: 26,
+      position: [53, 915],
+      maxWidth: 610,
     },
-    "type": {
-      "font": "cn",
-      "fontSize": 48,
-      "position": [750, 185],
-      "icon": [667, 147],
-      "iconSize": [46, 46]
+    type: {
+      font: "cn",
+      fontSize: 48,
+      position: [750, 185],
+      icon: [667, 147],
+      iconSize: [46, 46],
     },
-    "ATK": {
-      "font": "number",
-      "fontSize": 36,
-      "position": [585, 1107]
+    ATK: {
+      font: "number",
+      fontSize: 36,
+      position: [585, 1107],
     },
-    "DEF": {
-      "font": "link",
-      "fontSize": 36,
-      "position": [750, 1107]
+    DEF: {
+      font: "link",
+      fontSize: 36,
+      position: [750, 1107],
     },
-    "pendulumNumber": {
-      "font": "number",
-      "fontSize": 52,
-      "positonLeft": [88, 854],
-      "positonRight": [730, 854]
+    pendulumNumber: {
+      font: "number",
+      fontSize: 52,
+      positonLeft: [88, 854],
+      positonRight: [730, 854],
     },
-    "linkArrows": {
-      "center": [408, 528],
-      "arrow1": {
-        "size": [140, 41],
-        "position": [-70, 308]
+    linkArrows: {
+      center: [408, 528],
+      arrow1: {
+        size: [140, 41],
+        position: [-70, 308],
       },
-      "arrow2": {
-        "size": [70, 70],
-        "position": [-336, 267]
-      }
+      arrow2: {
+        size: [70, 70],
+        position: [-336, 267],
+      },
     },
-    "cardbag": {
-      "font": "password",
-      "fontSize": 24,
-      "position": [728, 871],
-      "linkPosition": [665, 872],
-      "pendulumPosition": [66, 1104],
+    cardbag: {
+      font: "password",
+      fontSize: 24,
+      position: [728, 871],
+      linkPosition: [665, 872],
+      pendulumPosition: [66, 1104],
     },
-    "password": {
-      "font": 'password',
-      "fontSize": 23,
-      "position": [40, 1149]
+    password: {
+      font: "password",
+      fontSize: 23,
+      position: [40, 1149],
     },
-    "holo": {
-      "position": [743, 1115],
-      "size": [42, 42]
-    }
+    holo: {
+      position: [743, 1115],
+      size: [42, 42],
+    },
   },
-  "translate": {
-    "attribute": {
-      "light": '光', "dark": '暗', "wind": '风', "water": '水', "fire": '炎', "earth": '地', "divine": '神'
+  translate: {
+    attribute: {
+      light: "光",
+      dark: "暗",
+      wind: "风",
+      water: "水",
+      fire: "炎",
+      earth: "地",
+      divine: "神",
     },
-    "type": {
-      "tc": '通常', "xg": '效果', "ys": '儀式', "rh": '融合', "tk": '衍生物', "tt": '同調', "cl": '超量', "lb": '靈擺', "lj": '連接', "ec": '二重', "tz": '調整', "tm": '同盟', "kt": '卡通', "lh": '靈魂', "fz": '反轉', "ts": '特殊召喚', "zb": '裝備', "sg": '速攻', "cd": '場地', "fj": '反擊', "yx": '永續'
+    type: {
+      tc: "通常",
+      xg: "效果",
+      ys: "儀式",
+      rh: "融合",
+      tk: "衍生物",
+      tt: "同調",
+      cl: "超量",
+      lb: "靈擺",
+      lj: "連接",
+      ec: "二重",
+      tz: "調整",
+      tm: "同盟",
+      kt: "卡通",
+      lh: "靈魂",
+      fz: "反轉",
+      ts: "特殊召喚",
+      zb: "裝備",
+      sg: "速攻",
+      cd: "場地",
+      fj: "反擊",
+      yx: "永續",
     },
-    "raceList": ["龍族","戰士族","惡魔族","魔法師族","天使族","不死族","岩石族","植物族","昆蟲族","水族","炎族","雷族","魚族","海龍族","幻龍族","恐龍族","爬蟲类族","機械族","獸族","鳥獸族","獸戰士族","念動力族","電子界族","幻神獸族","創造神族"],
-    "spell": "魔法卡",
-    "trap": "陷阱卡",
-    "brackets": ['【' , '】']
-  }
-}
+    raceList: [
+      "龍族",
+      "戰士族",
+      "惡魔族",
+      "魔法師族",
+      "天使族",
+      "不死族",
+      "岩石族",
+      "植物族",
+      "昆蟲族",
+      "水族",
+      "炎族",
+      "雷族",
+      "魚族",
+      "海龍族",
+      "幻龍族",
+      "恐龍族",
+      "爬蟲类族",
+      "機械族",
+      "獸族",
+      "鳥獸族",
+      "獸戰士族",
+      "念動力族",
+      "電子界族",
+      "幻神獸族",
+      "創造神族",
+    ],
+    spell: "魔法卡",
+    trap: "陷阱卡",
+    brackets: ["【", "】"],
+  },
+};
--- dist/config/defaultEnConfig.js
+++ dist/config/defaultEnConfig.js
@@ -1,144 +1,196 @@
 export default {
-  "moldName": "default",
-  "fonts": {
-    "en_name": {
-      "name": "en_name.ttf",
-      "type": "relative"
+  moldName: "default",
+  fonts: {
+    en_name: {
+      name: "en_name.ttf",
+      type: "relative",
     },
-    "en": {
-      "name": "en.ttf",
-      "type": "relative"
+    en: {
+      name: "en.ttf",
+      type: "relative",
     },
-    "number": {
-      "name": "number.ttf",
-      "type": "relative"
+    number: {
+      name: "number.ttf",
+      type: "relative",
     },
-    "link": {
-      "name": "link.ttf",
-      "type": "relative"
+    link: {
+      name: "link.ttf",
+      type: "relative",
     },
-    "race": {
-      "name": "race.ttf",
-      "type": "relative"
+    race: {
+      name: "race.ttf",
+      type: "relative",
     },
-    "password": {
-      "name": "password.ttf",
-      "type": "relative"
-    }
+    password: {
+      name: "password.ttf",
+      type: "relative",
+    },
   },
-  "style": {
-    "moldSize": [813, 1185],
-    "name": {
-      "font": "en_name",
-      "fontSize": 65,
-      "maxWidth": 610,
-      "position": [65, 102]
+  style: {
+    moldSize: [813, 1185],
+    name: {
+      font: "en_name",
+      fontSize: 65,
+      maxWidth: 610,
+      position: [65, 102],
     },
-    "pic": {
-      "position": [101, 220, 614, 616],
-      "position_lb": [57, 213, 702, 528]
+    pic: {
+      position: [101, 220, 614, 616],
+      position_lb: [57, 213, 702, 528],
     },
-    "attribute": {
-      "position": [680, 57, 75, 75],
-      "size": [66, 66]
+    attribute: {
+      position: [680, 57, 75, 75],
+      size: [66, 66],
     },
-    "level": {
-      "position": [686, 145],
-      "size": [50, 50],
-      "distance": 55
+    level: {
+      position: [686, 145],
+      size: [50, 50],
+      distance: 55,
     },
-    "monsterDesc": {
-      "splitMode": "en",
-      "italic": true,
-      "font": "en",
-      "fontSize": 24,
-      "lbFontSize": 22,
-      "position": [66, 950],
-      "lbPosition": [130, 773],
-      "lineHeight": 25,
-      "lbLineHeight": 24.5,
-      "maxLines": 6,
-      "maxWidth": 683
+    monsterDesc: {
+      splitMode: "en",
+      italic: true,
+      font: "en",
+      fontSize: 24,
+      lbFontSize: 22,
+      position: [66, 950],
+      lbPosition: [130, 773],
+      lineHeight: 25,
+      lbLineHeight: 24.5,
+      maxLines: 6,
+      maxWidth: 683,
     },
-    "spellDesc": {
-      "splitMode": "en",
-      "italic": true,
-      "font": "en",
-      "fontSize": 24,
-      "position": [66, 915],
-      "lineHeight": 24,
-      "maxLines": 9,
-      "maxWidth": 683
+    spellDesc: {
+      splitMode: "en",
+      italic: true,
+      font: "en",
+      fontSize: 24,
+      position: [66, 915],
+      lineHeight: 24,
+      maxLines: 9,
+      maxWidth: 683,
     },
-    "race": {
-      "font": "race",
-      "fontSize": 32,
-      "fontWieght": "bold",
-      "position": [64, 920],
-      "maxWidth": 610
+    race: {
+      font: "race",
+      fontSize: 32,
+      fontWieght: "bold",
+      position: [64, 920],
+      maxWidth: 610,
     },
-    "type": {
-      "font": "race",
-      "fontSize": 48,
-      "position": [737, 185],
-      "icon": [675, 147],
-      "iconSize": [46, 46]
+    type: {
+      font: "race",
+      fontSize: 48,
+      position: [737, 185],
+      icon: [675, 147],
+      iconSize: [46, 46],
     },
-    "ATK": {
-      "font": "number",
-      "fontSize": 36,
-      "position": [585, 1107]
+    ATK: {
+      font: "number",
+      fontSize: 36,
+      position: [585, 1107],
     },
-    "DEF": {
-      "font": "link",
-      "fontSize": 36,
-      "position": [750, 1107]
+    DEF: {
+      font: "link",
+      fontSize: 36,
+      position: [750, 1107],
     },
-    "pendulumNumber": {
-      "font": "number",
-      "fontSize": 52,
-      "positonLeft": [88, 854],
-      "positonRight": [730, 854],
-      "pendulumPosition": [66, 1102],
+    pendulumNumber: {
+      font: "number",
+      fontSize: 52,
+      positonLeft: [88, 854],
+      positonRight: [730, 854],
+      pendulumPosition: [66, 1102],
     },
-    "linkArrows": {
-      "center": [408, 528],
-      "arrow1": {
-        "size": [140, 41],
-        "position": [-70, 308]
+    linkArrows: {
+      center: [408, 528],
+      arrow1: {
+        size: [140, 41],
+        position: [-70, 308],
       },
-      "arrow2": {
-        "size": [70, 70],
-        "position": [-336, 267]
-      }
+      arrow2: {
+        size: [70, 70],
+        position: [-336, 267],
+      },
     },
-    "cardbag": {
-      "font": "password",
-      "fontSize": 24,
-      "position": [728, 871],
-      "linkPosition": [665, 872],
-      "pendulumPosition": [66, 1104],
+    cardbag: {
+      font: "password",
+      fontSize: 24,
+      position: [728, 871],
+      linkPosition: [665, 872],
+      pendulumPosition: [66, 1104],
     },
-    "password": {
-      "font": 'password',
-      "fontSize": 23,
-      "position": [40, 1149]
+    password: {
+      font: "password",
+      fontSize: 23,
+      position: [40, 1149],
     },
-    "holo": {
-      "position": [743, 1115],
-      "size": [42, 42]
-    }
+    holo: {
+      position: [743, 1115],
+      size: [42, 42],
+    },
   },
-  "translate": {
-    "attribute": {
-      "light": '光', "dark": '暗', "wind": '风', "water": '水', "fire": '炎', "earth": '地', "divine": '神'
+  translate: {
+    attribute: {
+      light: "光",
+      dark: "暗",
+      wind: "风",
+      water: "水",
+      fire: "炎",
+      earth: "地",
+      divine: "神",
     },
-    "type": {
-      "tc": 'normal', "xg": 'effect', "ys": 'ritual', "rh": 'fusion', "tk": 'fusion', "tt": 'synchro', "cl": 'xyz', "lb": 'pendulum', "lj": 'link', "ec": 'gemini', "tz": 'tuner', "tm": 'union', "kt": 'toon', "lh": 'spirit', "fz": 'flip effect', "ts": 'special summon', "zb": 'equip', "sg": 'quick-play', "cd": 'field', "fj": 'counter', "yx": 'continuous'
+    type: {
+      tc: "normal",
+      xg: "effect",
+      ys: "ritual",
+      rh: "fusion",
+      tk: "fusion",
+      tt: "synchro",
+      cl: "xyz",
+      lb: "pendulum",
+      lj: "link",
+      ec: "gemini",
+      tz: "tuner",
+      tm: "union",
+      kt: "toon",
+      lh: "spirit",
+      fz: "flip effect",
+      ts: "special summon",
+      zb: "equip",
+      sg: "quick-play",
+      cd: "field",
+      fj: "counter",
+      yx: "continuous",
     },
-    "raceList": ["Dragon", "Warrior", "Fiend", "Spellcaster", "Fairy", "Zombie", "Rock", "Plant", "Insect", "Aqua", "Pyro", "Thunder", "Fish", "Sea Serpent", "Wyrm", "Dinosaur", "Reptile", "Machine", "Beast", "Winged Beast", "Beast-Warrior", "Psychic", "Cyberse", "Divine-Beast", "Divine"],
-    "spell": "Spell Card",
-    "tragic": "Trap Card",
-    "brackets": ['[' , ']']
-  }
-}
+    raceList: [
+      "Dragon",
+      "Warrior",
+      "Fiend",
+      "Spellcaster",
+      "Fairy",
+      "Zombie",
+      "Rock",
+      "Plant",
+      "Insect",
+      "Aqua",
+      "Pyro",
+      "Thunder",
+      "Fish",
+      "Sea Serpent",
+      "Wyrm",
+      "Dinosaur",
+      "Reptile",
+      "Machine",
+      "Beast",
+      "Winged Beast",
+      "Beast-Warrior",
+      "Psychic",
+      "Cyberse",
+      "Divine-Beast",
+      "Divine",
+    ],
+    spell: "Spell Card",
+    tragic: "Trap Card",
+    brackets: ["[", "]"],
+  },
+};
