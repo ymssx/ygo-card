913af040a05f6c6bb385aea891bd00788e6f4c05
Yami <ymssx@qq.com>
2021-01-02 22:26:11 +0800

    feat: 11期版权

--- source/lib/copyright.js
+++ source/lib/copyright.js
@@ -2,5 +2,5 @@ export default {
   jp: 'ⓒ高橋和希 スタジオ・ダイス/集英社',
   en: 'ⓒ1996 KAZUKI TAKAHASHI',
   cn: 'ⓒ2020 Studio Dice/SHUEISHA, TV TOKYO,KONAMI',
-  new: 'ⓒスタジオ・ダイス/集英社·',
+  new: 'ⓒスタジオ·ダイス /集英社·テレビ東京·KONAMI',
 };
